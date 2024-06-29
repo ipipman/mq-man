801c2b15dbb061c8ba6ae5758210a679a0393024
ipipman <ipipman@163.com>
2024-06-29 19:11:49 +0800

    add springboot framework

---
+++ src/test/java/cn/ipman/mq/MqManApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ipman.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
