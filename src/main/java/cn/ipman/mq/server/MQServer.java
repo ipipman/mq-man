13b66e11403ea9dbb0bebe8272ba5853dc76250d
ipipman <ipipman@163.com>
2024-06-30 20:21:14 +0800

    switch spring web

---
+++ src/main/java/cn/ipman/mq/server/MQServer.java
@@ -0,0 +1,28 @@
+package cn.ipman.mq.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description for this class
+ *
+ * @Author IpMan
+ * @Date 2024/6/30 20:17
+ */
+@Controller
+@RequestMapping("/mq")
+public class MQServer {
+
+
+    // send
+
+    // receive
+
+    // ack
+
+    // subscribe
+
+    // unsubscribe
+
+
+}
